8c6a4974937b631fc7d3c951ef936676c076b6e5
Eduardo Hernandez <eduardo@chatbot-plus.com>
2021-09-11 12:11:56 +0200

    add image support to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,25 @@ if (process.env.NODE_ENV === "production") {
 module.exports = {
   mode: mode,
   target: target,
+  output : {
+    assetModuleFilename: "images/[hash][ext][query]",
+  },
   module: {
     rules :[
       {
+        test: /\.(png|jpe?g|gif|svg)ss$/i,
+        type: "asset",
+      },
+      {
         test:  /\.(s[ac]|c)ss$/i,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" }
+          },
+          "css-loader", 
+          "postcss-loader", 
+          "sass-loader"],
       },
       {
         test: /\.jsx?$/,
