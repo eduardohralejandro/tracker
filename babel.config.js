062d6c370daf00c1a5d6f57b62a727e70eb48f48
Eduardo Hernandez <eduardo@chatbot-plus.com>
2021-09-13 21:48:16 +0200

    connect real data with map, first test

--- babel.config.js
+++ babel.config.js
@@ -3,4 +3,5 @@ module.exports = {
   ["@babel/preset-react", { runtime: "automatic"}], 
   "@babel/preset-typescript"
 ],
+plugins: ["@babel/plugin-transform-runtime"]
 }
---
+++ src/components/utils/data.ts
@@ -0,0 +1,37 @@
+const cities = [
+  {
+    name: 'brussels',
+    coordinates: [50.85628104510911, 4.343033412604632],
+  },
+  {
+    name: 'New york',
+    coordinates: [40.728377663787185, -74.09232524793403] 
+  },
+  {
+    name: 'beijing',
+    coordinates: [39.98170390893767, 116.36446514600281] 
+  },
+  {
+    name: 'Madrid',
+    coordinates: [40.4168, 3.7038] 
+  },
+  {
+    name: 'Barcelona',
+    coordinates: [41.390205, 2.154007] 
+  },
+  {
+    name: 'Rome',
+    coordinates: [41.9028, 12.4964] 
+  },
+  {
+    name: 'Paris',
+    coordinates: [48.8566, 2.3522] 
+  },
+  {
+    name: 'berlin',
+    coordinates: [52.5200, 13.4050] 
+  },
+]
+
+
+export default cities;
