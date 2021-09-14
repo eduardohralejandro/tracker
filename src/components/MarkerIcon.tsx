b932936c40cdfb468be00430a45740e1ddb7c9a7
Eduardo Hernandez <eduardo@chatbot-plus.com>
2021-09-14 20:36:11 +0200

    marker with respective color

--- src/components/MarkerIcon.tsx
+++ src/components/MarkerIcon.tsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, {FC} from 'react';
+
+import '../styles/index.scss';
 
 
-const MarkerIcon = () => {
+interface typeProps {
+  qualityIndex: number
+}
+
+const MarkerIcon: FC<typeProps> = ({ qualityIndex }) => {
+ 
+  const colorsIndex = {
+    good: '#57cc99',
+    modarate: "#ffee88",
+    sensitive: "#e3843b",
+    unhealthy: "#FF0035",
+    veryUnhealthy: "#4D2D76",
+    hazardous: "#6F1A07"
+  }
+  
+  const colorLogic = 
+  (qualityIndex <= 50 ? colorsIndex.good : qualityIndex  > 50  ? colorsIndex.modarate : null) ||  
+  (qualityIndex > 100 ? colorsIndex.sensitive : qualityIndex  > 150  ? colorsIndex.unhealthy : null) ||
+  (qualityIndex > 200 ? colorsIndex.veryUnhealthy : qualityIndex  > 300 ? colorsIndex.hazardous : null)
   return (
     <div>
-      marker
+      <button className='points' style={{backgroundColor: `${colorLogic}`}} />
     </div>
   );
 }
